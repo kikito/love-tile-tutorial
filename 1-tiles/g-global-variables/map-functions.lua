db4b08193b858f7517728a1d7418296343a949f2
Enrique García <kikito@gmail.com>
2011-01-02 12:57:36 +0100

    updated 1g code

--- 1-tiles/g-global-variables/map-functions.lua
+++ 1-tiles/g-global-variables/map-functions.lua
@@ -1,35 +1,37 @@
+-- declaring local variables that will be used only inside map-functions.lua
+local tileW, tileH, tileset, quads, tileTable
+
 function loadMap(path)
   love.filesystem.load(path)() -- attention! extra parenthesis
 end
 
-function newMap(tileW, tileH, tilesetPath, tileString, quadInfo)
-  Map = {}
+function newMap(tileWidth, tileHeight, tilesetPath, tileString, quadInfo)
   
-  Map.tileW = tileW
-  Map.tileH = tileH
-  Map.tileset = love.graphics.newImage(tilesetPath)
+  tileW = tileWidth
+  tileH = tileHeight
+  tileset = love.graphics.newImage(tilesetPath)
   
-  local tilesetW, tilesetH = Map.tileset:getWidth(), Map.tileset:getHeight()
+  local tilesetW, tilesetH = tileset:getWidth(), tileset:getHeight()
   
-  Map.quads = {}
+  quads = {}
   
   for _,info in ipairs(quadInfo) do
     -- info[1] = the character, info[2] = x, info[3] = y
-    Map.quads[info[1]] = love.graphics.newQuad(info[2], info[3], Map.tileW,  Map.tileH, tilesetW, tilesetH)
+    quads[info[1]] = love.graphics.newQuad(info[2], info[3], tileW,  tileH, tilesetW, tilesetH)
   end
   
-  Map.tiles = {}
+  tileTable = {}
   
   local width = #(tileString:match("[^\n]+"))
 
-  for x = 1,width,1 do Map.tiles[x] = {} end
+  for x = 1,width,1 do tileTable[x] = {} end
 
   local x,y = 1,1
   for row in tileString:gmatch("[^\n]+") do
     assert(#row == width, 'Map is not aligned: width of row ' .. tostring(y) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
     x = 1
     for tile in row:gmatch(".") do
-      Map.tiles[x][y] = tile
+      tileTable[x][y] = tile
       x = x + 1
     end
     y=y+1
@@ -38,9 +40,10 @@ function newMap(tileW, tileH, tilesetPath, tileString, quadInfo)
 end
 
 function drawMap()
-  for x,column in ipairs(Map.tiles) do
-    for y,char in ipairs(column) do
-      love.graphics.drawq(Map.tileset, Map.quads[ char ] , (x-1)*Map.tileW, (y-1)*Map.tileH)
+  for columnIndex,column in ipairs(tileTable) do
+    for rowIndex,char in ipairs(column) do
+      local x,y = (columnIndex-1)*tileW, (rowIndex-1)*tileH
+      love.graphics.drawq(tileset, quads[ char ] , x, y)
     end
   end
 end
