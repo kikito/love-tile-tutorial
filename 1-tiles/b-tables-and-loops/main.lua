4d06abf9eb65d98ece68b22a79b3d9ed7d172a29
Derek <zorfie123@gmail.com>
2014-06-19 15:02:26 +0100

    Renamed vairables to be in accordance with the Lua Style Guide

--- 1-tiles/b-tables-and-loops/main.lua
+++ 1-tiles/b-tables-and-loops/main.lua
@@ -2,20 +2,20 @@
 
 function love.load()
 
-  TileW, TileH = 32,32
+  tileW, tileH = 32,32
   
-  Tileset = love.graphics.newImage('countryside.png')
+  tileSet = love.graphics.newImage('countryside.png')
   
-  local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
+  local tilesetW, tilesetH = tileSet:getWidth(), tileSet:getHeight()
   
-  Quads = {
-    love.graphics.newQuad(0,   0, TileW, TileH, tilesetW, tilesetH), -- 1 = grass
-    love.graphics.newQuad(32,  0, TileW, TileH, tilesetW, tilesetH), -- 2 = box
-    love.graphics.newQuad(0,  32, TileW, TileH, tilesetW, tilesetH), -- 3 = flowers
-    love.graphics.newQuad(32, 32, TileW, TileH, tilesetW, tilesetH)  -- 4 = boxtop
+  quads = {
+    love.graphics.newQuad(0,   0, tileW, tileH, tilesetW, tilesetH), -- 1 = grass
+    love.graphics.newQuad(32,  0, tileW, tileH, tilesetW, tilesetH), -- 2 = box
+    love.graphics.newQuad(0,  32, tileW, tileH, tilesetW, tilesetH), -- 3 = flowers
+    love.graphics.newQuad(32, 32, tileW, tileH, tilesetW, tilesetH)  -- 4 = boxtop
   }
   
-  TileTable = {
+  tileTable = {
   
      { 4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4 },
      { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,4 },
@@ -40,13 +40,13 @@ function love.load()
 end
 
 function love.draw()
-  for rowIndex=1, #TileTable do
-    local row = TileTable[rowIndex]
+  for rowIndex=1, #tileTable do
+    local row = tileTable[rowIndex]
     for columnIndex=1, #row do
       local number = row[columnIndex]
-      local x = (columnIndex-1)*TileW
-      local y = (rowIndex-1)*TileH
-      love.graphics.draw(Tileset, Quads[number], x, y)
+      local x = (columnIndex-1)*tileW
+      local y = (rowIndex-1)*tileH
+      love.graphics.draw(tileSet, quads[number], x, y)
     end
   end
 end
